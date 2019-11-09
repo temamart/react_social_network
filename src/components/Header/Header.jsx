6c928b5c2d3332662b249d1ddae0ed2286b3f99b
Artem Martynenko <artem.martynenko@djangostars.com>
2019-11-09 19:15:02 +0200

    01-Start project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src='https://colorlib.com/wp/wp-content/uploads/sites/2/react-dev-tools-logo.jpg'/>
+        </header>
+    )
+};
+
+export default Header;
