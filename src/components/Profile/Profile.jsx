84f507ea13767a038772ed142651062dc7a7548f
Artem Martynenko <artem.martynenko@djangostars.com>
2019-11-10 15:52:05 +0200

    06-Dialogs components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://chto-eto-takoe.ru/uryaimg/32574385521dd1847f7d1e5b940491ef.jpg'/>
             </div>
