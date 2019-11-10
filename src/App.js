117e4ebf83e04da0229c2c8d7ef95b8e10b6ced0
Artem Martynenko <artem.martynenko@djangostars.com>
2019-11-10 17:45:06 +0200

    07-NavLink

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Header from './components/Header/Header';
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import './App.css';
 import Dialogs from "./components/Dialogs/Dialogs";
 
-const App = () => {
+const App = (props) => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                {/*<Profile/>*/}
-                <Dialogs/>
+        <Router>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
             </div>
-        </div>
+        </Router>
     )
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
-                <a>Profile</a>
+                <NavLink to='/profile'>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a>Messages</a>
+                <NavLink to='/dialogs'>Messages</NavLink>
             </div>
-            <div className={s.item}>
-                <a>News</a>
-            </div>
-            <div className={s.item}>
-                <a>Music</a>
-            </div>
-            <div className={s.item}>
-                <a>Settings</a>
-            </div>
+            {/*<div className={s.item}>*/}
+            {/*    <a href='/news'>News</a>*/}
+            {/*</div>*/}
+            {/*<div className={s.item}>*/}
+            {/*    <a href='/music'>Music</a>*/}
+            {/*</div>*/}
+            {/*<div className={s.item}>*/}
+            {/*    <a href='/settings'>Settings</a>*/}
+            {/*</div>*/}
         </nav>
     )
 };
