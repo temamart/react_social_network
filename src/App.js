e283e3c67a660423a8390c88c760a49432f1eece
Artem Martynenko <artem.martynenko@djangostars.com>
2019-11-25 00:26:42 +0200

    09-Route exact

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = (props) => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
+                    <Route exact path='/dialogs' component={Dialogs}/>
+                    <Route exact path='/profile' component={Profile}/>
                 </div>
             </div>
         </Router>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,19 @@
 import React from "react"
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog}>
-                    Artem
+                    <NavLink to='/dialogs/1'>Artem</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Dima
+                    <NavLink to='/dialogs/2'>Dima</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Oleg
+                    <NavLink to='/dialogs/3'>Oleg</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
