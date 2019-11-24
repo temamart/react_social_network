e12cfde7f5be860563c6c3434cc493942fe5b507
Artem Martynenko <artem.martynenko@djangostars.com>
2019-11-24 23:59:18 +0200

    08-Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,25 @@
 import React from "react"
+import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Artem
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Oleg
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Aloha</div>
+                <div className={s.message}>Hello</div>
+            </div>
         </div>
     )
 };
